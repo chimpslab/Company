c7825d5d55985b30a3dd31737d8d624230ce337a
besanta <nicosanta@gmail.com>
2023-01-11 11:44:24 +0100

    fine tag subsystem

--- src/client/core/tags.ts
+++ src/client/core/tags.ts
@@ -5,25 +5,27 @@ export class PTTagInput extends HTMLElement {
     o: { delimiter: string; };
     tmpl_tag_list: string;
     input: HTMLInputElement;
+    output: HTMLInputElement;
     tooltip: HTMLElement;
 
     connectedCallback() {
         this.o = {delimiter: ","};
         this.tmpl_tag_list = "#tmpl_ulist";
-        this.input = this.querySelector<HTMLInputElement>("input");
+        this.input = this.querySelector<HTMLInputElement>("#tagsin");
+        this.output = this.querySelector<HTMLInputElement>("#tagsout");
         this.tooltip = this.querySelector<HTMLElement>('[data-tooltip]');
         this.addEventListener("change", () => this.update());
 
         this.update();
     }
     update(){
-        const val = this.input.value;
+        const val = this.output.value + this.o.delimiter +  this.input.value;
         const words = _.chain(val.split(this.o.delimiter))
             .map(x => x.trim())
             .uniq()
             .compact()
             .value();
-        this.input.value = words.join(this.o.delimiter);
+        this.output.value = words.join(this.o.delimiter);
         const $new_list = $(Mustache.render(
             $(this.tmpl_tag_list).html(), 
             {
@@ -42,13 +44,14 @@ export class PTTagInput extends HTMLElement {
             });
         });
 
+        this.input.value = "";
         this.tooltip.classList.toggle("collapse", t.length > 0);
     }
 
     removetag(term: string) {
-        const val = this.input.value;
+        const val = this.output.value;
         const words = _.uniq(val.split(this.o.delimiter).map(x => x.trim()));
-        this.input.value = _.chain(words)
+        this.output.value = _.chain(words)
             .filter(k => k != term)
             .join(this.o.delimiter)
             .value();
