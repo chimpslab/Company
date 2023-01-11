93e69be99aca9a22d3d67cb0037ebfba5fbe8d9c
besanta <nicosanta@gmail.com>
2023-01-11 10:57:06 +0100

    tag management

--- src/client/core/tags.ts
+++ src/client/core/tags.ts
@@ -3,25 +3,18 @@ import Mustache from "mustache";
 
 export class PTTagInput extends HTMLElement {
     o: { delimiter: string; };
-    tmpl_tag: string;
     tmpl_tag_list: string;
     input: HTMLInputElement;
+    tooltip: HTMLElement;
 
     connectedCallback() {
-        const options = {};
         this.o = {delimiter: ","};
         this.tmpl_tag_list = "#tmpl_ulist";
-        this.tmpl_tag = "#tmpl_tags";
         this.input = this.querySelector<HTMLInputElement>("input");
-    
-        if (!$.isPlainObject(options)) {
-            this.o = Object.assign({delimiter: ","}, options || {});
-        }
+        this.tooltip = this.querySelector<HTMLElement>('[data-tooltip]');
         this.addEventListener("change", () => this.update());
 
         this.update();
-    }
-    destroy() {
     }
     update(){
         const val = this.input.value;
@@ -32,21 +25,24 @@ export class PTTagInput extends HTMLElement {
             .value();
         this.input.value = words.join(this.o.delimiter);
         const $new_list = $(Mustache.render(
-            $(this.tmpl_tag_list).html(), {
+            $(this.tmpl_tag_list).html(), 
+            {
                 tags: words,
-            }, {
-                tag: $(this.tmpl_tag).html(),
-            })
-        );
-        $(this).next(".tags-list").remove();
-        $(this).after($new_list);
+            }
+        ));
+        $(this).find(".tags-list").remove();
+        $(this).append($new_list);
         $(this).trigger("tags_changed");
-        $new_list.find("[data-action]").toArray().forEach(tagbtn => {
+
+        const t = this.querySelectorAll<HTMLElement>("[data-bs-dismiss]");
+        _.forEach(t, tagbtn => {
             tagbtn.addEventListener("click", l => {
-                l.preventDefault();
+                l.stopPropagation();
                 this.removetag(tagbtn.dataset.value);
             });
         });
+
+        this.tooltip.classList.toggle("collapse", t.length > 0);
     }
 
     removetag(term: string) {
